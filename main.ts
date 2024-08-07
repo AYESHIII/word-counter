9d66ed0adb1249f557d0f01e249e9e4ae1f9cfb1
Ayesha Sheikh <160367523+AYESHIII@users.noreply.github.com>
2024-08-08 03:39:57 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,27 @@
+//#! user/bin/env node 
+// importing inquirer and chalk packages 
+import inquirer from "inquirer";
+import chalk from "chalk"
+ 
+//dispaly a colorful welcome message
+console.log (chalk.cyanBright.bold("\n\t WELCOME TO SHEIKS WORD COUNTER \n\t"));
+console.log ("=".repeat(60));
+
+// promt the user to enter the sentence
+let answers = await inquirer.prompt([
+    {
+        name : "sentence",
+        type : "input",
+        message : "enter a sentence",
+    }
+]);
+
+// triming the sentence and split it into a words base on " space"
+let words = answers.sentence.trim().split(" ");
+
+// analysis user input mssage
+console.log ("=".repeat(60));
+console.log(chalk.bold("- sentence words:"));
+console.log(words);
+console.log (chalk.bold(`\n- word count : ${chalk.blue.bold (words.length)}`));
+console.log ("=".repeat(60));
